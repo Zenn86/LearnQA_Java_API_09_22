0a585532f99dc32f817730298166e1ff7b1c99f3
Boris Zenkov <Zenn86z@gmail.com>
2022-09-26 18:41:38 +0300

    Excersize 5

--- src/test/java/Ex5.java
+++ src/test/java/Ex5.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Ex5 {
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+
+public class Ex5 {
+
+    @Test
+    public void testJsonParsing() {
+        JsonPath response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_json_homework")
+                .jsonPath();
+
+        List<JsonPath> answer = response.getList("messages");
+        System.out.println(answer.get(1));
+    }
+
 }
