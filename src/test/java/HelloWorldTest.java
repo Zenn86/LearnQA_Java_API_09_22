0caff7da56c14cf252587cae7a5f70de98909ca3
Boris Zenkov <Zenn86z@gmail.com>
2022-09-26 18:42:10 +0300

    some minor changes

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,6 +1,5 @@
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
-
 import org.junit.jupiter.api.Test;
 
 public class HelloWorldTest {
--- src/test/java/RestAssuredFirstTest.java
+++ src/test/java/RestAssuredFirstTest.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class RestAssuredFirstTest {
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class RestAssuredFirstTest {
+
+    @Test
+    public void testRestAssured() {
+
+        Response response = RestAssured
+                .given()
+                .queryParam("param1", "value1")
+                .queryParam("param2", "value2")
+                .post("https://playground.learnqa.ru/api/check_type")
+                .andReturn();
+
+        response.print();
+    }
+
 }
