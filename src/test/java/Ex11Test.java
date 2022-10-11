8ac1d3db981ae06e55b44fe11543e656667683e4
Boris Zenkov <Zenn86z@gmail.com>
2022-10-11 19:50:22 +0300

    code refactor

--- src/test/java/Ex11Test.java
+++ src/test/java/Ex11Test.java
@@ -1,17 +1,16 @@
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import lib.BaseTestCase;
 import org.junit.jupiter.api.Test;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class Ex11Test {
-
+public class Ex11Test extends BaseTestCase {
+    String url = "https://playground.learnqa.ru/api/homework_cookie";
     @Test
     public void testCookie() {
-        Response response = RestAssured
-                .get("https://playground.learnqa.ru/api/homework_cookie")
-                .andReturn();
+        Response response = getResponce(url);
         assertFalse(response.getCookies().isEmpty(), "There is no cookies");
         Map<String, String> cookies = response.getCookies();
         Response response2 = RestAssured
