5a3df5a0493e61ae10d94f676db33429f0f645c8
Boris Zenkov <Zenn86z@gmail.com>
2022-12-15 18:32:53 +0300

    update clarity of code

--- src/test/java/tests/UserRegisterTest.java
+++ src/test/java/tests/UserRegisterTest.java
@@ -2,7 +2,9 @@ package tests;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import lib.Assertions;
 import lib.BaseTestCase;
+import lib.DataGenerator;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
@@ -11,12 +13,11 @@ import java.util.Map;
 public class UserRegisterTest extends BaseTestCase {
     @Test
     public void testCreateUserWithExistingEmail() {
+        String email = "vinkotov@example.com";
+
         Map<String, String> userData = new HashMap<>();
-        userData.put("email", "vinkotov@example.com");
-        userData.put("password", "123");
-        userData.put("username", "learnqa");
-        userData.put("firstName", "learnqa");
-        userData.put("lastName", "learnqa");
+        userData.put("email", email);
+        userData = DataGenerator.getRegistrationData(userData);
 
         Response responseCreateAuth = RestAssured
                 .given()
@@ -24,9 +25,23 @@ public class UserRegisterTest extends BaseTestCase {
                 .post("https://playground.learnqa.ru/api/user/")
                 .andReturn();
 
-        System.out.println(responseCreateAuth.asString());
-        System.out.println(responseCreateAuth.statusCode());
+        Assertions.assertResponseCodeEquals(responseCreateAuth, 400);
+        Assertions.assertResponseTextEquals(responseCreateAuth, "Users with email '" + email + "' already exists");
+    }
 
+    @Test
+    public void testCreateUserSuccesfully() {
+
+        Map<String, String> userData = DataGenerator.getRegistrationData();
+
+        Response responseCreateAuth = RestAssured
+                .given()
+                .body(userData)
+                .post("https://playground.learnqa.ru/api/user/")
+                .andReturn();
+
+        Assertions.assertResponseCodeEquals(responseCreateAuth, 200);
+        Assertions.assertJsonHasField(responseCreateAuth, "id");
     }
 
 }
