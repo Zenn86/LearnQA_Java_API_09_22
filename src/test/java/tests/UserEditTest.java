4720b0b2f1960a1b84433747123face2f37048a7
Boris Zenkov <Zenn86z@gmail.com>
2022-12-15 18:33:55 +0300

    Add new working classes: Data Generator, UserGetTest and not working UserEditTest

---
+++ src/test/java/tests/UserEditTest.java
@@ -0,0 +1,11 @@
+package tests;
+
+import lib.BaseTestCase;
+import org.junit.jupiter.api.Test;
+
+public class UserEditTest extends BaseTestCase {
+    @Test
+    public void testEditJustCreated() {
+
+    }
+}
