4f1ec8949fa527d79910453071731d0947829b44
Boris Zenkov <Zenn86z@gmail.com>
2022-12-31 08:28:00 +0300

    Ex 17 fully works

--- src/test/java/tests/UserEditTest.java
+++ src/test/java/tests/UserEditTest.java
@@ -141,4 +141,36 @@ public class UserEditTest extends BaseTestCase {
         Assertions.assertResponseCodeEquals(responseEditUser, 400);
         Assertions.assertResponseTextEquals(responseEditUser,"Invalid email format");
     }
+
+    @Test
+    @Description("This test tries to edit userName just created user with a not valid value")
+    @DisplayName("Test negative edit userName with one symbol name")
+    public void testEditJustCreatedUserWithOneSymbolUserName() {
+        //GENERATE USER
+        Map<String, String> userData = DataGenerator.getRegistrationData();
+
+        JsonPath responseCreateAuth = apiCoreRequests.
+                makePostRequest("https://playground.learnqa.ru/api/user/", userData).jsonPath();
+
+        String userId = responseCreateAuth.getString("id");
+
+        //LOGIN
+        Map<String, String> authData = userData;
+
+        Response responseGetAuth = apiCoreRequests.
+                makePostRequest("https://playground.learnqa.ru/api/user/login", authData);
+
+        //EDIT
+        Map<String, String> editData = new HashMap<>();
+        editData.put("username", "I");
+        String header = getHeader(responseGetAuth, "x-csrf-token");
+        String cookie = getCookie(responseGetAuth,"auth_sid");
+
+        Response responseEditUser = apiCoreRequests
+                .makePutRequest("https://playground.learnqa.ru/api/user/" + userId, editData,
+                        header, cookie);
+
+        Assertions.assertResponseCodeEquals(responseEditUser, 400);
+        Assertions.assertJsonByName(responseEditUser, "error", "Too short value for field username");
+    }
 }
