c33284753c430ea279b36965042a2df0a238a771
Boris Zenkov <Zenn86z@gmail.com>
2022-12-31 08:18:21 +0300

    Ex 17 task 3 works and optimize recent code

--- src/test/java/tests/UserEditTest.java
+++ src/test/java/tests/UserEditTest.java
@@ -28,12 +28,7 @@ public class UserEditTest extends BaseTestCase {
         String userId = responseCreateAuth.getString("id");
 
         //LOGIN
-        Map<String, String> authData = new HashMap<>();
-        authData.put("email", userData.get("email"));
-        authData.put("password", userData.get("password"));
-        authData.put("username", userData.get("username"));
-        authData.put("firstName", userData.get("firstName"));
-        authData.put("lastName", userData.get("lastName"));
+        Map<String, String> authData = userData;
 
         Response responseGetAuth = apiCoreRequests.
                 makePostRequest("https://playground.learnqa.ru/api/user/login", authData);
@@ -112,5 +107,38 @@ public class UserEditTest extends BaseTestCase {
 
         Assertions.assertResponseCodeEquals(responseEditUser, 400);
         Assertions.assertResponseTextEquals(responseEditUser, "Please, do not edit test users with ID 1, 2, 3, 4 or 5.");
+    }
+
+    @Test
+    @Description("This test tries to edit email just created user with a not valid email value")
+    @DisplayName("Test negative edit user with invalid email")
+    public void testEditJustCreatedUserWithInvalidEmail() {
+        //GENERATE USER
+        Map<String, String> userData = DataGenerator.getRegistrationData();
+
+        JsonPath responseCreateAuth = apiCoreRequests.
+                makePostRequest("https://playground.learnqa.ru/api/user/", userData).jsonPath();
+
+        String userId = responseCreateAuth.getString("id");
+
+        //LOGIN
+        Map<String, String> authData = userData;
+
+        Response responseGetAuth = apiCoreRequests.
+                makePostRequest("https://playground.learnqa.ru/api/user/login", authData);
+
+        //EDIT
+        String invalidEmail = DataGenerator.getRandomEmail().replace('@', '.');
+        Map<String, String> editData = new HashMap<>();
+        editData.put("email", invalidEmail);
+        String header = getHeader(responseGetAuth, "x-csrf-token");
+        String cookie = getCookie(responseGetAuth,"auth_sid");
+
+        Response responseEditUser = apiCoreRequests
+                .makePutRequest("https://playground.learnqa.ru/api/user/" + userId, editData,
+                        header, cookie);
+
+        Assertions.assertResponseCodeEquals(responseEditUser, 400);
+        Assertions.assertResponseTextEquals(responseEditUser,"Invalid email format");
     }
 }
