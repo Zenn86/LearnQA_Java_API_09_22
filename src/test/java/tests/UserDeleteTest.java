12c4f118046eb45c1f4c668bb74992d82c719d97
Boris Zenkov <Zenn86z@gmail.com>
2022-12-31 09:33:21 +0300

    Ex 18 fully works

--- src/test/java/tests/UserDeleteTest.java
+++ src/test/java/tests/UserDeleteTest.java
@@ -17,7 +17,7 @@ public class UserDeleteTest {
 
     @Test
     @Description("This test tries to delete user, who can't be deleted")
-    @DisplayName("Test negative delete user")
+    @DisplayName("Test negative delete user, who can't be deleted")
     public void testDeleteUserWithUserId2() {
         //LOGIN
         Map<String, String> authData = new HashMap<>();
@@ -65,5 +65,40 @@ public class UserDeleteTest {
         Assertions.assertResponseTextEquals(responseAfterDeleting, "User not found");
     }
 
+    @Test
+    @Description("This test tries to delete user with another user authorization")
+    @DisplayName("Test positive delete user with another user's authorization")
+    public void testDeleteUserWithAnotherUserAuth() {
+        //GENERATE USER
+        Map<String, String> userData = DataGenerator.getRegistrationData();
+
+        JsonPath responseCreateAuth = apiCoreRequests.
+                makePostRequest("https://playground.learnqa.ru/api/user/", userData).jsonPath();
+
+        String userId = responseCreateAuth.getString("id");
+        System.out.println(userId);
+
+        //LOGIN
+        Map<String, String> authData = new HashMap<>();
+        authData.put("email", "vinkotov@example.com");
+        authData.put("password", "1234");
+
+        Response responseGetAuth = apiCoreRequests.makePostRequest("https://playground.learnqa.ru/api/user/login", authData);
+
+        //DELETE
+        Response responseDeleteUser = apiCoreRequests
+                .makeDeleteRequest("https://playground.learnqa.ru/api/user/" + userId,
+                        responseGetAuth.getHeader("x-csrf-token"), responseGetAuth.getCookie("auth_sid"));
+        Assertions.assertResponseCodeEquals(responseDeleteUser, 400);
+        Assertions.assertResponseTextEquals(responseDeleteUser,
+                "Please, do not delete test users with ID 1, 2, 3, 4 or 5.");
+
+        //GET
+        Response responseAfterDeleting = apiCoreRequests
+                .makeGetRequestWithoutTokenAndCookie("https://playground.learnqa.ru/api/user/" + userId);
+
+        Assertions.assertResponseCodeEquals(responseAfterDeleting, 200);
+        Assertions.assertJsonByName(responseAfterDeleting, "username", userData.get("username"));
+    }
 
 }
